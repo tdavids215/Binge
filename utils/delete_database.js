4584e35663206ef944e71b8b85f13a5111831857
Sam <Sambose426@yahoo.com>
2023-10-12 18:22:32 -0600

    added all of the models to the delete function

--- utils/delete_database.js
+++ utils/delete_database.js
@@ -1,5 +1,8 @@
 const sequelize = require('../config/connection');
 const User = require('../models/User');
+const Book = require('../models/Book');
+const Song = require('../models/Song');
+const Movie = require('../models/Movie');
 
 // -----> uncomment each function call to either delete all data or show all data in the user model <-----
 
@@ -16,6 +19,15 @@ async function deleteAllData() {
 		await sequelize.sync({});
 		console.log('connection synced');
 		await User.destroy({
+			where: {},
+		});
+		await Book.destroy({
+			where: {},
+		});
+		await Song.destroy({
+			where: {},
+		});
+		await Movie.destroy({
 			where: {},
 		});
 		console.log('All data has been deleted');
